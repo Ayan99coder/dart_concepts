2f402d6c7d1832b559a6f5f93269bd208105ac9a
Ayan Javed <ayanjaved803@gmail.com>
2026-02-14 01:09:06 +0500

    Initial commit

--- lib/programs.dart
+++ lib/programs.dart
@@ -1,4 +1,3 @@
-
 import 'dart:math';
 
 void main() {
@@ -20,21 +19,87 @@ void main() {
 
   // Area of a circle
   double radius = 5;
-  double area = pi*radius*radius;
+  double area = pi * radius * radius;
   print('The area of a circle is $area');
 
-// area of a square
+  // area of a square
   double side = 4;
 
   double areaofsq = side * side;
 
   print("Area of Square: $areaofsq");
 
-// area of a rectangle
+  // area of a rectangle
   double length = 5;
   double width = 4;
   double areaofrec = length * width;
   print("Area of Rectangle: $areaofrec");
+  // even and odd number check
+  int number = 5;
+  if (number % 2 == 0) {
+    print("The number is even");
+  } else {
+    print('the number is odd');
+  }
+  // the letter is consonent or vowel
+  String name = 'a';
+  if ('aeiou'.contains(name)) {
+    print('The letter is vowel');
+  } else {
+    print('The letter is consonant');
+  }
+  // check the number is positive or negative
+  int num = 1;
+  if (num >= 0) {
+    print('The number is positive');
+  } else {
+    print('The number is negative');
+  }
+  // 100 times name
+
+  String name1 = 'Ayan';
+
+  for (int i = 1; i <= 100; i++) {
+    print('$i : $name1 ');
+  }
+  // Sum of natural numbers
+  int sum = 0;
+  for (int i = 1; i <= 100; i++) {
+    sum += i;
+  }
+  print('The sum of natural numbers is $sum');
 
 
+// table of 5
+  int n = 5;
+  for (int i = 1; i <=10; i++) {
+    int result = n * i;
+    print('$n * $i = $result');
+  }
+//table from one to nine
+
+    for (int i = 1; i <= 9; i++) {
+      print("Table of $i");
+
+      for (int j = 1; j <= 10; j++) {
+        int result = i * j;
+        print("$i x $j = $result");
+      }
+
+      print("------------");
+    }
+for (int i = 1; i <= 100;i++){
+  if(i == 41){
+    continue;
+    }
+  print(i);
+}
+display();
+print(displays());
+}
+void display(){
+  print('AYAN');
+}
+String displays(){
+   return 'Ayan';
 }

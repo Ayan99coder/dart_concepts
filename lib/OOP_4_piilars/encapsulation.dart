77a9885a05d86c1cc29cf51fbd30dbfae41862f8
Ayan Javed <ayanjaved803@gmail.com>
2026-02-15 00:41:50 +0500

    Initial commit

--- lib/OOP_4_piilars/encapsulation.dart
+++ lib/OOP_4_piilars/encapsulation.dart
@@ -15,49 +15,104 @@ class Person {
 
   void setId(int id) => this._id = id;
 }
-class Vehicle{
-   String? _model;
-   int? _year;
+
+class Vehicle {
+  String? _model;
+  int? _year;
+
   String get model => _model!;
+
   int get year => _year!;
+
   set model(String model) => _model = model;
+
   set year(int year) => _year = year;
-
-
 }
 
 class Person2 {
   String? _firstName;
   String? _secondName;
-  Person2(this._firstName, this._secondName);
- String get fullNmae => '$_firstName $_secondName';
 
+  Person2(this._firstName, this._secondName);
+
+  String get fullNmae => '$_firstName $_secondName';
 }
-class NoteBook{
+
+class NoteBook {
   final String? _title;
   final int? _prize;
+
   NoteBook(this._title, this._prize);
+
   String get title => _title!;
+
   int get prize => _prize!;
+
   dynamic get info => '$_title : $_prize';
 }
 
 class Doctor {
-
   String _name;
   int _age;
   String _gender;
 
-
   Doctor(this._name, this._age, this._gender);
 
-
   String get name => _name;
+
   int get age => _age;
+
   String get gender => _gender;
 
   Map<String, dynamic> get map {
     return {"name": _name, "age": _age, "gender": _gender};
+  }
+}
+
+class Student {
+  String? _firstName;
+  String? _lastName;
+  int? _age;
+
+  String get firstName => _firstName!;
+
+  String get lastName => _lastName!;
+
+  String get fullNmae => this._firstName! + " " + this._lastName!;
+
+  int get age => _age!;
+
+  set fristName(String firstName) => this._firstName = firstName;
+
+  set lastName(String lastName) => this._lastName = lastName;
+
+  set age(int age) {
+    if (age < 0) {
+      print('Invalid age');
+    }
+    this._age = age;
+  }
+}
+
+class BankAccount {
+
+  double _balance = 0.0;
+
+
+  double get balance => this._balance;
+
+
+  void deposit(double amount) {
+    this._balance += amount;
+  }
+
+
+  void withdraw(double amount) {
+    if (this._balance >= amount) {
+      this._balance -= amount;
+    } else {
+      throw new Exception("Insufficient Balance");
+    }
   }
 }
 void main() {
@@ -80,4 +135,16 @@ void main() {
 
   Doctor doctor = Doctor('Ayan', 21, 'Male');
   print(doctor.map);
+
+  Student student = Student();
+  student.fristName = 'Ayan';
+  student.lastName = 'Haseeb';
+  student.age = 21;
+  print('${student.firstName} ${student.lastName} : ${student.age}');
+
+  BankAccount bankAccount = BankAccount();
+  bankAccount.deposit(1000);
+  print(bankAccount.balance);
+  bankAccount.withdraw(500);
+  print(bankAccount.balance);
 }
